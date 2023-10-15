1504d0f77b9bd3be551ff1e3e07fb3e7b2719690
Yuliya Kuzmich <kuzmich.yuliya@gmail.com>
2023-10-15 12:21:19 +0200

    feat: add styles and start button

--- random-game/script.js
+++ random-game/script.js
@@ -1,3 +1,4 @@
+const startButton = document.getElementById('start-button');
 const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
@@ -7,6 +8,32 @@ let result = 0;
 let hitPosition;
 let currentTime = 30;
 let timerId = null;
+let gameInterval = null;
+
+function startGame() {
+  result = 0;
+  currentTime = 30;
+  score.textContent = result;
+  timeLeft.textContent = currentTime;
+
+  clearInterval(timerId); // Clear the existing mole movement
+  clearInterval(gameInterval); // Clear the countdown timer
+
+  moveMole(); // Start the mole movement
+  gameInterval = setInterval(countDown, 1000); // Start the countdown timer
+  startButton.disabled = true; // Disable the Start button while the game is in progress
+}
+
+function stopGame() {
+  clearInterval(timerId); // Clear the mole movement
+  clearInterval(gameInterval); // Clear the countdown timer
+  startButton.disabled = false; // Enable the Start button
+  alert('GAME OVER! Your final score is ' + result);
+  storeScore(result); // Store the current score
+  displayLast10Scores(); // Update the scores list
+}
+
+startButton.addEventListener('click', startGame);
 
 function randomSquare() {
   squares.forEach(square => {
@@ -33,22 +60,14 @@ function moveMole() {
   timerId = setInterval(randomSquare, 750);
 }
 
-moveMole();
-
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime;
 
   if (currentTime == 0) {
-    clearInterval(countDownTimerId);
-    clearInterval(timerId);
-    alert('GAME OVER! Your final score is ' + result);
-    storeScore(result); // Store the current score
-    displayLast10Scores(); // Update the scores list
+    stopGame(); // End the game when time runs out
   }
 }
-
-let countDownTimerId = setInterval(countDown, 1000);
 
 function storeScore(score) {
   const scores = JSON.parse(localStorage.getItem('scores')) || [];
@@ -68,7 +87,7 @@ function displayLast10Scores() {
 
   last10Scores.forEach((score, index) => {
     const listItem = document.createElement('li');
-    listItem.textContent = `Game ${index + 1}: ${score}`;
+    listItem.textContent = `Game ${index + 1} :  ${score} scores`;
     scoresList.appendChild(listItem);
   });
 }
